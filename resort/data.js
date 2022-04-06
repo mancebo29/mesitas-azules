c584b1e7f72b489c9aaa134d3ce83ac0760a3e2d
Jairo Mancebo <jairolmr@hotmail.com>
2022-04-06 18:02:59 -0400

    Add resort info WIP

---
+++ resort/data.js
@@ -0,0 +1,177 @@
+window.resort = [
+  {
+    "name": "Danna Ledesma",
+    "roomType": 1,
+    "paymentType": 6
+  },
+  {
+    "name": "César Gabriel Rondón",
+    "roomType": 1,
+    "paymentType": 1
+  },
+  {
+    "name": "Franchesca Rodríguez",
+    "roomType": 2,
+    "paymentType": 4
+  },
+  {
+    "name": "Joadys Lora",
+    "roomType": 2,
+    "paymentType": 6
+  },
+  {
+    "name": "Enmanuel Polanco",
+    "roomType": 2,
+    "paymentType": 4
+  },
+  {
+    "name": "Mariela Nouel",
+    "roomType": 2,
+    "paymentType": 4
+  },
+  {
+    "name": "Gregory Santos",
+    "roomType": 2,
+    "paymentType": 6
+  },
+  {
+    "name": "Beatriz Fernández",
+    "roomType": 2,
+    "paymentType": 6
+  },
+  {
+    "name": "Davmmgi Vetaldiablo",
+    "roomType": 2,
+    "paymentType": 6
+  },
+  {
+    "name": "Marlyn Sánchez",
+    "roomType": 2,
+    "paymentType": 1
+  },
+  {
+    "name": "Miguel Anibal",
+    "roomType": 2,
+    "paymentType": 4
+  },
+  {
+    "name": "José Ricardo Martinez",
+    "roomType": 2,
+    "paymentType": 3
+  },
+  {
+    "name": "Adrián Silvestre",
+    "roomType": 2,
+    "paymentType": 4
+  },
+  {
+    "name": "Daniel Rodríguez",
+    "roomType": 2,
+    "paymentType": 1
+  },
+  {
+    "name": "Aris Pamela",
+    "roomType": 2,
+    "paymentType": 6
+  },
+  {
+    "name": "Lucille Henriquez",
+    "roomType": 2,
+    "paymentType": 6
+  },
+  {
+    "name": "Romer Díaz",
+    "roomType": 2,
+    "paymentType": 6
+  },
+  {
+    "name": "Cristal Abreu",
+    "roomType": 2,
+    "paymentType": 6
+  },
+  {
+    "name": "Luis Ángel",
+    "roomType": 2,
+    "paymentType": 4
+  },
+  {
+    "name": "Henry Vásquez",
+    "roomType": 2,
+    "paymentType": ""
+  },
+  {
+    "name": "Wilmor Mediina",
+    "roomType": 3,
+    "paymentType": 1
+  },
+  {
+    "name": "Jairo Mancebo",
+    "roomType": 3,
+    "paymentType": 1
+  },
+  {
+    "name": "Allan Taveras",
+    "roomType": 3,
+    "paymentType": 1
+  },
+  {
+    "name": "Richard González",
+    "roomType": 3,
+    "paymentType": ""
+  },
+  {
+    "name": "Camilo Osoria",
+    "roomType": 3,
+    "paymentType": 1
+  },
+  {
+    "name": "Miguel Roa",
+    "roomType": 3,
+    "paymentType": 6
+  },
+  {
+    "name": "Reudys Amador",
+    "roomType": 3,
+    "paymentType": 6
+  },
+  {
+    "name": "Nathalie Chevalier",
+    "roomType": 3,
+    "paymentType": 6
+  },
+  {
+    "name": "Holiviel Valdez",
+    "roomType": 3,
+    "paymentType": 1
+  },
+  {
+    "name": "Gilbert Peña",
+    "roomType": 3,
+    "paymentType": 6
+  },
+  {
+    "name": "Verónica Creales",
+    "roomType": 3,
+    "paymentType": 6
+  },
+  {
+    "name": "Luis Merino",
+    "roomType": 3,
+    "paymentType": 4
+  },
+  {
+    "name": "José Alejandro Jiménez (Vive 100)",
+    "roomType": 3,
+    "paymentType": 6
+  },
+  {
+    "name": "Lisbeth Toribio",
+    "roomType": 3,
+    "paymentType": ""
+  },
+  {
+    "name": "Melissa Mateo",
+    "roomType": 3,
+    "paymentType": ""
+  }
+];
---
+++ resort/index.js
@@ -0,0 +1,111 @@
+const paymentInfo = {
+  '1': 18348.00,
+  '2': 15012.00,
+  '3': 11787.20
+};
+
+const quotesMap = {
+  '6': [5, 6, 7, 8, 9, 10],
+  '4': [4, 6, 8, 10],
+  '3': [4, 7, 10],
+  '1': [10],
+};
+
+const roomTypes = {
+  '1': 'Sencilla',
+  '2': 'Doble',
+  '3': 'Triple',
+}
+
+$(document).ready(function() {
+  const dataContainer = document.querySelector('.data');
+  const select = $('#nameSelector');
+  let dataHtml = '';
+  const formatter = new Intl.NumberFormat('es-DO', {
+    style: 'currency',
+    currency: 'DOP',
+  });
+
+  select.innerHTML = '';
+  window.resort.forEach(p => {
+    const option = document.createElement('option');
+    option.value = p.name;
+    option.innerHTML = p.name;
+    select.append(option);
+  });
+
+  select.select2();
+
+  const getMonthName = (num) => {
+    const date = new Date(2022, 1, 1);
+    date.setMonth(num - 1);
+    const options = { month: 'long' };
+    return date.toLocaleDateString('es-ES', options);
+  }
+
+  const template = `
+        <div class="title">
+          <h1 id="name">@{name}</h1>
+        </div>
+        <div class="data-points">
+          <div class="entry">
+            <label>Tipo de habitación</label>
+            <span>@{roomType}</span>
+          </div>
+          <div class="entry">
+            <label>Plan de pago elegido</label>
+            <span>@{paymentInfo}</span>
+          </div>
+          <div class="entry">
+            <label>Tienes hasta <span>@{month}</span> para pagar: </label>
+            <span class="amount">@{pendingAmount}</span>
+          </div>
+          <div class="entry">
+            <label>Después de ahí @{quotesCount} @{missingMonths}</label>
+          </div>
+        </div>
+        <div class="accountInfo">
+          <p>BHD LEON</p>
+          <p>Nombre: Romer Diaz</p>
+          <p>Cta de ahorro: 30195400016 <button class="btn">Copiar</button></p>
+          <p>ID: 402-2401-371-0</p>
+        </div>
+`;
+
+  select.change((e) => {
+    const person = window.resort.find(p => p.name === e.target.value);
+    if (!person) {
+      console.log('Error');
+    }
+
+    console.log(person);
+
+    const nextMonth = quotesMap[person.paymentType].find(x => x >= new Date().getMonth() + 1);
+
+    const missingMonths = quotesMap[person.paymentType].filter(x => x > nextMonth);
+    dataHtml = template.replace(/@\{(\w+)\}/g, (match, g1) => {
+      switch (g1) {
+        case 'name':
+          return person.name;
+        case 'paymentInfo':
+          return `${person.paymentType} cuotas`;
+        case 'roomType':
+          return roomTypes[person.roomType];
+        case 'month':
+          return getMonthName(nextMonth);
+        case 'pendingAmount':
+          return formatter.format(paymentInfo[person.roomType] / person.paymentType);
+        case 'quotesCount':
+          return missingMonths.length ? `te faltarían ${missingMonths.length} cuotas: ` : 'ya.';
+        case 'missingMonths':
+          return missingMonths.length ? 'A pagar en ' + missingMonths.map(m => getMonthName(m)).join(', ') : '';
+      }
+    });
+
+    dataContainer.innerHTML = dataHtml;
+    $('.btn').click(() => {
+      console.log('clicked')
+      navigator.clipboard.writeText('30195400016').then(() => alert('copiado')).catch(() => alert('no se puede xD'));
+    });
+  });
+});
