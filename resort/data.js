0adf6cd20ce6af39d2bc500c8027e01b445abba7
Jairo Mancebo <jairolmr@hotmail.com>
2022-05-04 12:16:18 -0400

    Update resort data

--- resort/data.js
+++ resort/data.js
@@ -2,176 +2,262 @@ window.resort = [
   {
     "name": "Danna Ledesma",
     "roomType": 1,
+    "roomId": 1,
     "paymentType": 6
   },
   {
     "name": "César Gabriel Rondón",
     "roomType": 1,
+    "roomId": 2,
     "paymentType": 1
-  },
-  {
-    "name": "Franchesca Rodríguez",
-    "roomType": 2,
-    "paymentType": 4
   },
   {
     "name": "Joadys Lora",
     "roomType": 2,
+    "roomId": 14,
     "paymentType": 6
   },
   {
     "name": "Enmanuel Polanco",
     "roomType": 2,
+    "roomId": 4,
     "paymentType": 4
   },
   {
     "name": "Mariela Nouel",
     "roomType": 2,
+    "roomId": 4,
     "paymentType": 4
   },
   {
     "name": "Gregory Santos",
     "roomType": 2,
+    "roomId": 5,
     "paymentType": 6
   },
   {
     "name": "Beatriz Fernández",
     "roomType": 2,
+    "roomId": 5,
     "paymentType": 6
   },
   {
     "name": "Davmmgi Vetaldiablo",
     "roomType": 2,
+    "roomId": 6,
     "paymentType": 6
   },
   {
     "name": "Marlyn Sánchez",
     "roomType": 2,
+    "roomId": 6,
     "paymentType": 1
   },
   {
     "name": "Miguel Anibal",
     "roomType": 2,
+    "roomId": 7,
     "paymentType": 4
   },
   {
     "name": "José Ricardo Martinez",
     "roomType": 2,
+    "roomId": 7,
     "paymentType": 3
   },
   {
     "name": "Adrián Silvestre",
     "roomType": 2,
+    "roomId": 8,
     "paymentType": 4
   },
   {
     "name": "Daniel Rodríguez",
     "roomType": 2,
+    "roomId": 8,
     "paymentType": 1
   },
   {
-    "name": "Aris Pamela",
-    "roomType": 2,
+    "name": "Adis Pamela",
+    "roomType": 3,
+    "roomId": 16,
     "paymentType": 6
   },
   {
     "name": "Lucille Henriquez",
-    "roomType": 2,
+    "roomType": 3,
+    "roomId": 16,
     "paymentType": 6
+  },
+  {
+    "name": "Isaac Henriquez",
+    "roomType": 3,
+    "roomId": 16,
+    "paymentType": null
   },
   {
     "name": "Romer Díaz",
     "roomType": 2,
+    "roomId": 9,
     "paymentType": 6
   },
   {
     "name": "Cristal Abreu",
     "roomType": 2,
+    "roomId": 9,
     "paymentType": 6
   },
   {
     "name": "Luis Ángel",
     "roomType": 2,
+    "roomId": 10,
     "paymentType": 4
   },
   {
     "name": "Henry Vásquez",
     "roomType": 2,
+    "roomId": 10,
     "paymentType": 6
   },
   {
     "name": "Wilmor Medina",
     "roomType": 3,
+    "roomId": 11,
     "paymentType": 1
   },
   {
     "name": "Jairo Mancebo",
     "roomType": 3,
+    "roomId": 11,
     "paymentType": 1
   },
   {
     "name": "Allan Taveras",
     "roomType": 3,
+    "roomId": 11,
     "paymentType": 1
   },
   {
     "name": "Richard González",
     "roomType": 3,
+    "roomId": 12,
     "paymentType": 4
   },
   {
     "name": "Camilo Osoria",
     "roomType": 3,
+    "roomId": 12,
     "paymentType": 1
   },
   {
     "name": "Miguel Roa",
     "roomType": 3,
+    "roomId": 12,
     "paymentType": 6
   },
   {
-    "name": "Reudys Amador",
+    "name": "Amador Peña",
     "roomType": 3,
+    "roomId": 13,
     "paymentType": 6
   },
   {
     "name": "Nathalie Chevalier",
     "roomType": 3,
+    "roomId": 13,
     "paymentType": 6
   },
   {
     "name": "Holiviel Valdez",
     "roomType": 3,
+    "roomId": 13,
     "paymentType": 1
   },
   {
     "name": "Gilbert Peña",
     "roomType": 3,
+    "roomId": 14,
     "paymentType": 6
   },
   {
     "name": "Verónica Creales",
     "roomType": 3,
+    "roomId": 15,
     "paymentType": 6
   },
   {
     "name": "Luis Merino",
     "roomType": 3,
+    "roomId": 14,
     "paymentType": 4
   },
   {
     "name": "José Alejandro Jiménez (Vive 100)",
     "roomType": 3,
+    "roomId": 3,
     "paymentType": 6
   },
   {
     "name": "Lisbeth Toribio",
     "roomType": 3,
+    "roomId": 15,
     "paymentType": 6
   },
   {
     "name": "Melissa Mateo",
     "roomType": 3,
+    "roomId": 15,
     "paymentType": 6
   }
 ];
+
+window.rooms = {
+  '1': {
+    type: 1,
+  },
+  '2': {
+    type: 1,
+  },
+  '3': {
+    type: 1,
+  },
+  '4': {
+    type: 2,
+  },
+  '5': {
+    type: 2,
+  },
+  '6': {
+    type: 2,
+  },
+  '7': {
+    type: 2,
+  },
+  '8': {
+    type: 2,
+  },
+  '9': {
+    type: 2,
+  },
+  '10': {
+    type: 2,
+  },
+  '11': {
+    type: 3,
+  },
+  '12': {
+    type: 3,
+  },
+  '13': {
+    type: 3,
+  },
+  '14': {
+    type: 3,
+  },
+  '15': {
+    type: 3,
+  },
+  '16': {
+    type: 3,
+  },
+}
